26f2f5efbbd81631e3b89571fe8470bfaf1f7007
Missblue <mireille.dert@gmail.com>
2023-12-05 22:57:12 +0100

    remove submodule

---
+++ back/src/server.ts
@@ -0,0 +1,48 @@
+require("dotenv").config();
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { connectDB, sequelize } from "./db";
+import noteRouter from "./noteRoutes";
+import stageRouter from "./stageRoutes";
+import userRouter from "./userRoutes";
+
+const app = express();
+
+app.use(express.json({ limit: "10kb" }));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:19006"],
+    credentials: true,
+    exposedHeaders: 'X-Total-Count'
+  })
+);
+
+app.get("/api/healthchecker", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    message: "Build CRUD API with Node.js and Sequelize",
+  });
+});
+
+app.use("/api/notes", noteRouter);
+app.use("/api/users", userRouter);
+app.use("/api/stages", stageRouter);
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Route: ${req.originalUrl} does not exist on this server`,
+  });
+});
+
+const PORT = 8000;
+app.listen(PORT, async () => {
+  console.log("🚀Server started Successfully");
+  await connectDB();
+  sequelize.sync({ force: false }).then(() => {
+    console.log("✅Synced database successfully...");
+  });
+});
